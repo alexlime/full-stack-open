cefe9bab575fcd41d1f98e89a9369bc9981cff3d
alexlime <alexlimenko@gmail.com>
2022-09-14 02:12:49 +0100

    Add part5/bloglist-frontend ex 5.7*

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const visibility = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => { 
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} 
+      <button onClick={toggleVisibility}>{ visible ? 'hide' : 'show' }</button>
+      <div style={visibility}>      
+        {blog.url}<br />
+        lokes: {blog.likes} <button>like</button><br />
+        {blog.user.name}<br />
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
