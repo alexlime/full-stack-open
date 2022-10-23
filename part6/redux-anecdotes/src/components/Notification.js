6e6464a8aeca114065644de38585a306268017b4
alexlime <alexlimenko@gmail.com>
2022-10-23 19:45:03 +0100

    Add part6/redux-anecdotes ex 6.19

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,7 +1,9 @@
 import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  // const notification = useSelector(state => state.notification)
+  const notification = props.notification
 
   let style = {
     border: 'solid',
@@ -20,4 +22,12 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+// export default Notification
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
